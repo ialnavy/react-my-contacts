afcff558f42953b341799cc1e10a383623b547c4
ialnavy <ial.navy@protonmail.ch>
2024-04-25 19:28:12 +0200

    4.5. Obtención de la información de los contactos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=20')
+    .then(response => response.json())
+    .then(contacts => setContacts(contacts.results));
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Creating!</h1>
   );
 }
 
