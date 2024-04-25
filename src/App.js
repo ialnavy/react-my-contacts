fca187965901c43f9f316fe28093e0ddcf5fb048
ialnavy <ial.navy@protonmail.ch>
2024-04-25 19:52:37 +0200

    4.12. Posibilidad de ordenar los nombres en sentido ascendente o descendente

--- src/App.js
+++ src/App.js
@@ -24,6 +24,22 @@ function App() {
     return (contact.name['first'] + ' ' + contact.name['last'])
       .toLowerCase().includes(searchField.toLowerCase());
   });
+
+  const onAZ = () => {
+    let az = contacts.sort((a, b) => {
+      return (a.name['first'] + " " + a.name['last'])
+        .localeCompare(b.name['first'] + " " + b.name['last']);
+    });
+    setContacts([...az]); //clone the list
+  };
+  
+  const onZA = () => {
+    let za = contacts.sort((a, b) => {
+      return (b.name['first'] + " " + b.name['last'])
+        .localeCompare(a.name['first'] + " " + a.name['last']);
+    });
+    setContacts([...za]); //clone the list
+  };
   
   return (
     <div className='tc '>
@@ -33,7 +49,7 @@ function App() {
       {contacts.length === 0
         ? <h2 className='f2'>Loading...</h2>
         : (<ErrorBoundary FallbackComponent={ErrorFallback}>
-          <Searcher searchChange={onSearchChange}/>
+          <Searcher searchChange={onSearchChange} az={onAZ} za={onZA}/>
           <Scroll>
             <CardList contacts={searchedContacts}/>
           </Scroll>
