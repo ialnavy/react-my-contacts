52931fe790e277a2c9f42bd9fa3bf0c3c4aaa9ec
ialnavy <ial.navy@protonmail.ch>
2024-04-25 19:46:20 +0200

    4.10. Incorporación de un tipo de letra externo

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,10 +1,10 @@
 const Scroll = ({children}) => {
-    throw new Error('Testing error!');
-    /*
+    // throw new Error('Testing error!');
+
     return ( <div className="vh-75 ba bw2 br4 b--blue overflow-y-scroll">
         {children}
     </div> );
-    */
+
 };
 
 export default Scroll;
